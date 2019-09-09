37d6ff2d283f44b30a491bdb9cb5fa11650ee9ba
khandeshsailokesh <53443055+khandeshsailokesh@users.noreply.github.com>
2019-09-09 16:13:11 +0530

    Add files via upload

---
+++ lab_7.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int cnum(string X,string Y)
+{
+	string XY=X.append(Y);
+	string YX=Y.append(X);
+	return XY.compare(YX)>0 ? 1:0;
+};
+int main()
+{
+int t;
+vector<string> n;
+string s;
+cin>>t;
+	for(int i=0;i<t;i++)
+ 		{
+  			int r;
+  			cin>>r;
+  			for(int j=0;j<r;j++)
+			{
+				cin>>s;
+				n.push_back(s);
+			}
+			sort(n.begin(),n.end(),cnum);
+			for(int j=0;j<n.size();j++)
+			{
+				cout<<n[i];
+			}
+ 		}
+
+
+}
